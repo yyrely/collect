07fbc913a1fe5f47da5e9ad216a45b2d9df774c5
yyrely <734953453@qq.com>
2018-12-29 09:30:10 +0800

    CollectData导入

---
+++ collect/src/main/java/com/wanli/collect/dao/mapper/ext/CollectDataExtMapper.java
@@ -0,0 +1,8 @@
+package com.wanli.collect.dao.mapper.ext;
+
+import com.wanli.collect.dao.mapper.CollectDataMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CollectDataExtMapper extends CollectDataMapper {
+}
